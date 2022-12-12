7f558c521c46c22e5e1e1f1bcce5eacac66a3ecb
omeryildizce <omeryildizce@gmail.com>
2022-12-12 12:29:29 +0300

    variables

---
+++ bolum_01_degiskenler_ve_veri_tipleri/c02_variables/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        // reusability tekrar kullanılabilirlik
+        int ogrenciSayisi = 10;
+        String mesaj = "Öğrenci sayısı : ";
+        System.out.println(mesaj+ogrenciSayisi);
+        System.out.println(mesaj+ogrenciSayisi);
+
+        int sayi = 128;
+        byte sayi1 = 127;
+        double sayi2 = 127.0;
+        char karakter = '1';
+        System.out.println(karakter);
+        String string = "Ankara";
+        boolean dogruMu = false;
+        
+    }
+}
