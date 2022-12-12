1bc2a7566407a771e9f62b0c966d0a2d25f716f4
omeryildizce <omeryildizce@gmail.com>
2022-12-12 17:19:21 +0300

    while

---
+++ bolum_03_loop/c02_while/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 10)
+        {
+            System.out.print(i + " ");
+            i++;
+        }
+        System.out.println("");
+        do {
+            System.out.print(i+ " ");
+            i--;
+        }while (i>0);
+    }
+}
