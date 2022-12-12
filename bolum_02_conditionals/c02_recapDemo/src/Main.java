ee1134c551c573a4ba135bbeca83826e29806ab7
omeryildizce <omeryildizce@gmail.com>
2022-12-12 15:56:18 +0300

    Recap Demo

---
+++ bolum_02_conditionals/c02_recapDemo/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int sayi1 = 20;
+        int sayi2 = 25;
+        int sayi3 = 2;
+        int enbuyuk = sayi1;
+        if (sayi2> enbuyuk)
+        {
+            enbuyuk = sayi2;
+        }
+        if (sayi3 > enbuyuk) {
+            enbuyuk =sayi3;
+        }
+        System.out.println(enbuyuk);
+    }
+}
