9ecc9346851bf4e3464fdf514ce2ec76667d70b9
omeryildizce <omeryildizce@gmail.com>
2022-12-12 16:34:04 +0300

    for

---
+++ bolum_03_loop/c01_loop/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        for (int i = 1; i<10; i++)
+        {
+            System.out.println(i );
+        }
+        for (int i = 100; i>-1; i-=10)
+        {
+            System.out.print(i+" ");
+        }
+    }
+}
