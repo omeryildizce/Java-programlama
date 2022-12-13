d8797514ab67247941a01ae34b4febc53b9742c8
omeryildizce <omeryildizce@gmail.com>
2022-12-13 12:44:03 +0300

    overloading

---
+++ bolum_08_encapsulation/c02_overloading/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.topla(47,44));
+        calculator.topla(2,3,5);
+    }
+}
